7f6431efb56f7ada6277604c7d9956c3c724e0f5
Gerald Bolton <gboltondelivery@gmail.com>
2020-09-10 15:14:16 -0500

    First project review

---
+++ scripts/index.js
@@ -0,0 +1,35 @@
+const editButton = document.querySelector(".profile__edit-button");
+const closeButton = document.querySelector('.popup__close');
+const form = document.querySelector('.form');
+const modal = document.querySelector(".popup");
+const nameInput = document.querySelector('.form__input_name');
+const aboutInput = document.querySelector('.form__input_aboutme');
+const profileName = document.querySelector('.profile__name');
+const profileSubname = document.querySelector('.profile__title');
+const submitButton = document.querySelector(".popup__submit-button"); 
+
+
+
+
+
+form.addEventListener("submit",(event)=>{
+    event.preventDefault();
+
+profileName.textContent = nameInput.value;
+profileSubname.textContent = aboutInput.value;
+
+toggleModal()
+
+})
+
+function toggleModal(){
+    modal.classList.toggle("popup_opened");
+}
+
+editButton.addEventListener("click", toggleModal)
+
+closeButton.addEventListener("click", toggleModal)
+
+
+console.log(form)
+console.log(modal)
