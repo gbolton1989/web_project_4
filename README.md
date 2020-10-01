81fd895c8534ec5d674c706ae8cce281f30b9442
Gerald Bolton <gboltondelivery@gmail.com>
2020-10-01 11:02:09 -0500

    Project 5 Final

--- README.md
+++ README.md
@@ -2,9 +2,9 @@
 
 ### Overview
 
-A page where users can edit their profile info, made possible by the use of Javascript and a popup box. This also has multiple uses of various HTML and CSS techniques to add a user profile as well as a grid element to show off various photos of someone's journey.
+A page where users can edit their profile info and add new places they have pictures of, made possible by the use of Javascript and a popup box. This also has multiple uses of various HTML and CSS techniques to add a user profile as well as a grid element to show off various photos of someone's journey.
 
 
 https://gbolton1989.github.io/web_project_4/
 
-Fixed headings and other errors marked on review.
+I added more JS functionality to add new place elements as well as being able to like and delete place elements as well. 
--- scripts/index.js
+++ scripts/index.js
@@ -1,31 +1,144 @@
+//Wrappers
+const editProfileModalWindow = document.querySelector(".popup_edit-profile");
+const addElementModalWindow = document.querySelector(".popup_add-element");
+const imageModalWindow = document.querySelector(".popup_type_image");
+
+//Open buttons
 const editButton = document.querySelector(".profile__edit-button");
-const closeButton = document.querySelector('.popup__close');
-const form = document.querySelector('.form');
-const modal = document.querySelector(".popup");
-const nameInput = document.querySelector('.form__input_name');
-const aboutInput = document.querySelector('.form__input_aboutme');
+const addElementButton = document.querySelector(".profile__add-button");
+
+//Close buttons
+const modalCloseButton = editProfileModalWindow.querySelector(".popup__close");
+const closeAddElementModal = addElementModalWindow.querySelector(".popup__close");
+const closeImageModalWindow = imageModalWindow.querySelector(".popup__close");
+
+const editForm = document.querySelector(".form");
+const addElementForm = document.querySelector(".form__add-element");
+
+//Profile
+const nameInput = editForm.querySelector('.form__input_name');
+const aboutInput = editForm.querySelector('.form__input_aboutme');
 const profileName = document.querySelector('.profile__name');
 const profileSubname = document.querySelector('.profile__title');
-const submitButton = document.querySelector(".popup__submit-button"); 
+
+//Add form
+const elementTitleInput = document.querySelector(".form__input_title");
+const elementImageUrlInput = document.querySelector(".form__input_url");
 
 
-
-
-
-form.addEventListener("submit",(event)=>{
-    event.preventDefault();
-
-profileName.textContent = nameInput.value;
-profileSubname.textContent = aboutInput.value;
-
-toggleModal()
-
-})
-
-function toggleModal(){
-    modal.classList.toggle("popup_opened");
+function toggleModalWindow(modal){
+    modal.classList.toggle("popup_is-opened");
 }
 
-editButton.addEventListener("click", toggleModal)
+function formSubmitHandler(evt) {
+    evt.preventDefault();
+    profileName.textContent = nameInput.value;
+    profileSubname.textContent = aboutInput.value;
+    toggleModalWindow(editProfileModalWindow);
+}
 
-closeButton.addEventListener("click", toggleModal)
+editForm.addEventListener("submit", formSubmitHandler);
+editButton.addEventListener("click", () => {
+    if (!editProfileModalWindow.classList.contains("popup_is")) {
+        nameInput.value = profileName.textContent;
+        aboutInput.value = profileSubname.textContent;
+    }
+    toggleModalWindow(editProfileModalWindow);
+})
+
+
+modalCloseButton.addEventListener("click", () => {
+    toggleModalWindow(editProfileModalWindow);
+})
+
+addElementButton.addEventListener("click", () => {
+    toggleModalWindow(addElementModalWindow);
+})
+
+closeAddElementModal.addEventListener("click", () => {
+    toggleModalWindow(addElementModalWindow);
+})
+
+closeImageModalWindow.addEventListener("click", () => {
+    toggleModalWindow(imageModalWindow);
+})
+
+
+const initialCards = [
+{
+    name: "Yosemite Valley",
+    link: "https://code.s3.yandex.net/web-code/yosemite.jpg",
+},
+{
+    name: "Lake Louise",
+    link: "https://code.s3.yandex.net/web-code/lake-louise.jpg",
+},
+{
+    name: "Bald Mountains",
+    link: "https://code.s3.yandex.net/web-code/bald-mountains.jpg",
+},
+{
+    name: "Latemar",
+    link: "https://code.s3.yandex.net/web-code/latemar.jpg",
+},
+{
+    name: "Vanoise National Park",
+    link: "https://code.s3.yandex.net/web-code/vanoise.jpg",
+},
+{
+    name: "Lago di Braies",
+    link: "https://code.s3.yandex.net/web-code/lago.jpg",
+},
+];
+
+const elementTemplate = document.querySelector("#element-template").content.querySelector(".element");
+const list = document.querySelector(".element__list");
+
+initialCards.forEach((data) => {
+    list.prepend(createElement(data.name, data.link));
+});
+
+addElementForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+    list.prepend(createElement(elementTitleInput.value, elementImageUrlInput.value));
+    toggleModalWindow(addElementModalWindow);
+});
+
+
+function createElement(title, imageLink) {
+
+    const elementCard = elementTemplate.cloneNode(true);
+    const elementImage = elementCard.querySelector(".element__image");
+    const elementTitle = elementCard.querySelector(".element__place-title");
+    const elementHeart = elementCard.querySelector(".element__heart");
+    const elementDeleteButton = elementCard.querySelector(".element__delete");
+
+    elementTitle.textContent = title;
+    elementImage.src = imageLink;
+    elementImage.alt = title;
+
+    
+    elementHeart.addEventListener("click", function(evt) {
+        evt.target.classList.toggle("element__heart_active");
+    })
+
+    elementDeleteButton.addEventListener("click", function() {
+        const listItem = elementDeleteButton.closest('.element');
+        listItem.remove();
+    })
+
+    elementImage.addEventListener("click", () => {
+        const popupImage = imageModalWindow.querySelector(".popup__image");
+        const popupImageTitle = imageModalWindow.querySelector(".popup__image-title");
+
+        popupImage.src = imageLink;
+        popupImage.alt = title;
+        popupImageTitle.textContent = title;
+        
+        toggleModalWindow(imageModalWindow);                
+    });
+
+
+    return elementCard;
+}
+
